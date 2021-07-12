642327752671dbfd07f5edcfb6a34ff84cdcf620
guozhaoxuan <1175398345@qq.com>
2021-07-12 18:33:44 +0800

    构建核心代码

---
+++ src/OutApi/Appliaction.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by
+ * User: GuoZhaoXuan
+ * Date: 2021/7/12
+ * Time: 18:30
+ */
+
+namespace Vartruexuan\EasyOutApi\OutApi;
+
+use Vartruexuan\EasyOutApi\Kernel\ServiceContainer;
+
+class Appliaction extends ServiceContainer
+{
+
+
+}
